4eb4f12d1524f733d1ffd154b3d034f02a996627
Luis <luis.choque@whiz.pe>
2019-12-02 10:20:23 -0500

    Not working yet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
 
-console.log("test 1 ", process.env);
+const fs = require('fs');
+
+const path = require('path');
+
+const dir = (...relative) => {
+console.log(path.resolve(__dirname, ...relative));
+return path.resolve(__dirname, ...relative);}
+
+async function main (){
+    
+    fs.mkdirSync(dir('src'), { recursive: true });
+    fs.mkdirSync(dir('src', 'migrations'), { recursive: true });
+    fs.mkdirSync(dir('src', 'models'), { recursive: true });
+
+    const envExists = fs.existsSync(dir('.env'));
+    
+    if(envExists){
+        console.log(`
+            Add these lines to your .env:
+            
+            DB_DATABASE=
+            DB_HOST=
+            DB_USER=
+            DB_PASSWORD=
+        `);
+    } else {
+        fs.writeFileSync(dir('.env'), `DB_DATABASE=MyCustomDatabase
+DB_HOST=127.0.0.1
+DB_USER=root
+DB_PASSWORD=`)    
+    }
+    
+
+}
+
+
+main()
+.then( () => console.log("Done, success. ") )
+.catch( (e) => console.log("Error, try again later. ", e) )
+.finally( () => process.exit() );
